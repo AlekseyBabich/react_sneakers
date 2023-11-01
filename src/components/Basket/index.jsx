4d03ff893c3b6d638dc43cb1920469ecf1d1159a
Леха Омск <babichalex1986@gmail.com>
2023-11-01 19:14:10 +0600

    add emptyBasket

--- src/components/Basket/index.jsx
+++ src/components/Basket/index.jsx
@@ -1,44 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import CardBasket from "./CardBasket";
 import GreenBtn from "../Buttons/GreenBtn";
+import CloseBtn from "../Buttons/CloseBtn";
 
 import styles from './Basket.module.scss'
-import CloseBtn from "../Buttons/CloseBtn";
+import EmptyBasket from "./EmptyBasket";
 
 
 const Basket = ({ closeBasketTab, basketItems, removeItem }) => {
 
-
-
   return (
-    <div className={ styles.overlay } >
+    <div className={ styles.overlay }>
       <div className={ styles.basket }>
         <h2>
           Корзина
-          <CloseBtn close={closeBasketTab}/>
+          { basketItems.length > 0 ? <CloseBtn close={ closeBasketTab }/> : null }
         </h2>
-        <div className={styles.items}>
-          { basketItems.map(card => (
-            <CardBasket key={card.id} id={card.id} name={ card.name } price={ card.price } src={ card.src } removeItem={removeItem}/>
-          )) }
-        </div>
-
-        <div className={ styles.cartBottomBlock }>
-          <ul>
-            <li>
-              <span>Итого:</span>
-              <div></div>
-              <b>21498 руб.</b>
-            </li>
-            <li>
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
-
-          <GreenBtn name={'Оформить заказ'}/>
-
+        <div className={ styles.contentWrapper }>
+          { basketItems.length > 0
+            ? <div className={styles.itemsWrapper}>
+              <div className={ styles.items }>
+                { basketItems.map(card => (
+                  <CardBasket key={ card.id }
+                              id={ card.id }
+                              name={ card.name }
+                              price={ card.price }
+                              src={ card.src }
+                              removeItem={ removeItem }/>
+                )) }
+              </div>
+              <div className={ styles.cartBottomBlock }>
+                <ul>
+                  <li>
+                    <span>Итого:</span>
+                    <div></div>
+                    <b>21498 руб.</b>
+                  </li>
+                  <li>
+                    <span>Налог 5%:</span>
+                    <div></div>
+                    <b>1074 руб.</b>
+                  </li>
+                </ul>
+                <GreenBtn name={ 'Оформить заказ' }/>
+              </div>
+            </div>
+            : <EmptyBasket closeBasketTab={ closeBasketTab }/>
+          }
         </div>
       </div>
     </div>
--- src/components/Buttons/GreenBtn/index.jsx
+++ src/components/Buttons/GreenBtn/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import styles from './GreenBtn.module.scss'
 
-const GreenBtn = ({ name }) => {
+const GreenBtn = ({ name, onClickBtn }) => {
   return (
     <div>
-      <button className={styles.greenButton}>
+      <button className={styles.greenButton} onClick={onClickBtn}>
         <p>{name}</p>
         <img src="/img/arrow.svg" alt="arrow" width={ 13 } height={ 12 }/>
       </button>
