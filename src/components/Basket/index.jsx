1018b9278da6c37bff8261fdb0e0ef564c60056b
Леха Омск <babichalex1986@gmail.com>
2023-11-01 21:19:28 +0600

    add total price in basket

--- src/components/Basket/index.jsx
+++ src/components/Basket/index.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import CardBasket from "./CardBasket";
 import GreenBtn from "../Buttons/GreenBtn";
 import CloseBtn from "../Buttons/CloseBtn";
+import EmptyBasket from "./EmptyBasket";
 
 import styles from './Basket.module.scss'
-import EmptyBasket from "./EmptyBasket";
 
 
 const Basket = ({ closeBasketTab, basketItems, removeItem }) => {
+
+  const totalPrice = basketItems.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.price
+  }, 0)
+
+  const taxes = (totalPrice * 5) / 100
+
 
   return (
     <div className={ styles.overlay }>
@@ -17,35 +24,36 @@ const Basket = ({ closeBasketTab, basketItems, removeItem }) => {
           { basketItems.length > 0 ? <CloseBtn close={ closeBasketTab }/> : null }
         </h2>
         <div className={ styles.contentWrapper }>
-          { basketItems.length > 0
-            ? <div className={styles.itemsWrapper}>
-              <div className={ styles.items }>
-                { basketItems.map(card => (
-                  <CardBasket key={ card.id }
-                              id={ card.id }
-                              name={ card.name }
-                              price={ card.price }
-                              src={ card.src }
-                              removeItem={ removeItem }/>
-                )) }
+          {
+            basketItems.length > 0
+              ? <div className={ styles.itemsWrapper }>
+                <div className={ styles.items }>
+                  { basketItems.map(card => (
+                    <CardBasket key={ card.id }
+                                id={ card.id }
+                                name={ card.name }
+                                price={ card.price }
+                                src={ card.src }
+                                removeItem={ removeItem }/>
+                  )) }
+                </div>
+                <div className={ styles.cartBottomBlock }>
+                  <ul>
+                    <li>
+                      <span>Итого:</span>
+                      <div></div>
+                      <b>{ totalPrice } руб.</b>
+                    </li>
+                    <li>
+                      <span>Налог 5%:</span>
+                      <div></div>
+                      <b>{ taxes } руб.</b>
+                    </li>
+                  </ul>
+                  <GreenBtn name={ 'Оформить заказ' }/>
+                </div>
               </div>
-              <div className={ styles.cartBottomBlock }>
-                <ul>
-                  <li>
-                    <span>Итого:</span>
-                    <div></div>
-                    <b>21498 руб.</b>
-                  </li>
-                  <li>
-                    <span>Налог 5%:</span>
-                    <div></div>
-                    <b>1074 руб.</b>
-                  </li>
-                </ul>
-                <GreenBtn name={ 'Оформить заказ' }/>
-              </div>
-            </div>
-            : <EmptyBasket closeBasketTab={ closeBasketTab }/>
+              : <EmptyBasket closeBasketTab={ closeBasketTab }/>
           }
         </div>
       </div>
